b453c60e5d5ff87ab282bc1250f97f9ff405ecb8
Edward Skrod <edskrod@microsoft.com>
2022-10-08 19:36:49 -0400

    updated chinese language folder

--- build/PrepareStagingDirectoryForSelfExtractor.ps1
+++ build/PrepareStagingDirectoryForSelfExtractor.ps1
@@ -30,8 +30,8 @@ param(
 # the folder names used by the Group Policy editor in c:\Windows\GroupPolicy
 # Map the Microbuild Language outputs to the OS codes to store the resource files.
 $admxExtractorlanguageTable = [ordered]@{
-    CHS="zh-cn";
-    CHT="zh-tw";
+    CHS="zh-hans";
+    CHT="zh-hant";
     CSY="cs";
     DEU="de";
     ENU="en";
