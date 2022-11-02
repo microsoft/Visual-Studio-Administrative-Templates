12e7b035e4a805ac18d3714d32229cba73f591ba
Edward Skrod <edskrod@microsoft.com>
2022-11-02 13:50:51 -0400

    Updated the script's synopsis and description. (#34)

--- build/PrepareStagingDirectoryForSelfExtractor.ps1
+++ build/PrepareStagingDirectoryForSelfExtractor.ps1
@@ -1,9 +1,10 @@
 ﻿    <#
 .SYNOPSIS
-    Generates the self-extracting ADMXExtractor exe
+    Prepares a staging directory for consumption by the BoxStub tool.
 
 .DESCRIPTION
-    Zips up the admx and adml files, ADMXExtractor.exe using the box tool to create the self-extracting admin update exe.
+    Prepares a staging directory with the appropriate language directories, ADMX and ADML files 
+    for consumption by the BoxStub tool that creates the self-extracting VisualStudioAdminTemplates.exe.
 
 .PARAMETER $ArtifactsDir
     The root artifact directory where the ADMXExtractor build was generated.
