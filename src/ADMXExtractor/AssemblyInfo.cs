0d7f1d789166fceb335306fa06c142835494885d
Tyler Dunkel <40210514+tydunkel@users.noreply.github.com>
2024-05-31 20:00:20 +0000

    Add NeutralResourcesLanguage attribute to fix #1987586

--- src/ADMXExtractor/AssemblyInfo.cs
+++ src/ADMXExtractor/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 
+[assembly: NeutralResourcesLanguage("en")]
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
